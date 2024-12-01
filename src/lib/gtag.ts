6fc9b9fb13eaee4c6629148e67794b115795899b
developerhost <kanonhara0609@gmail.com>
2024-12-01 14:09:57 +0900

    feat: GAコンポーネントのページビュー送信ロジックを更新し、ページ遷移を監視するように変更

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -6,6 +6,7 @@ declare global {
 
 export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID;
 
+/** ページビューを送信 */
 export const pageview = (url: string) => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   window.gtag("config", GA_MEASUREMENT_ID ?? "", {
