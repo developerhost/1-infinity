7b4912aa643adb46331a349d7387a1919d9a186a
developerhost <kanonhara0609@gmail.com>
2024-12-01 11:19:58 +0900

    feat: GAを導入

---
+++ src/app/_components/GA.tsx
@@ -0,0 +1,40 @@
+import { useRouter } from "next/router";
+import Script from "next/script";
+import { useEffect } from "react";
+import * as gtag from "src/lib/gtag";
+
+export default function GA() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouterChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+
+           gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+           `,
+        }}
+      />
+    </>
+  );
+}
---
+++ src/lib/gtag.ts
@@ -0,0 +1,14 @@
+declare global {
+  interface Window {
+    gtag: (command: string, id: string, config: { page_path: string }) => void;
+  }
+}
+
+export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID;
+
+export const pageview = (url: string) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  window.gtag("config", GA_MEASUREMENT_ID ?? "", {
+    page_path: url,
+  });
+};
