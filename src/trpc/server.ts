658111a5d1be605a70282b243219912adc8e6150
developerhost <kanonhara0609@gmail.com>
2024-12-09 18:32:20 +0900

    feat: createCallerの型安全性を無効化するeslintルールを追加

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call */
+
 import "server-only";
 
 import { createHydrationHelpers } from "@trpc/react-query/rsc";
@@ -22,7 +24,7 @@ const createContext = cache(() => {
 });
 
 const getQueryClient = cache(createQueryClient);
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+
 const caller = createCaller(createContext);
 
 export const { trpc: api, HydrateClient } = createHydrationHelpers<AppRouter>(
